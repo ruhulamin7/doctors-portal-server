8bd40f2c9e0659606c55b66dd2d352570eb6909a
ruhulaminbd93 <ruhulaminbd93@gmail.com>
2021-11-09 01:15:17 +0600

    appointment post

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000
 
 // middleware
 app.use(cors())
-app.use(express())
+app.use(express.json())
 
 
 
@@ -21,7 +21,18 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
         await client.connect();
-        console.log('database connented successully!')
+        // console.log('database connented successully!')
+        const database = client.db("doctors_portal");
+        const appointmentsCollection = database.collection("appointments");
+
+        // create a document to insert
+        app.post('/appointments', async (req, res) => {
+            const appointment = req.body;
+            console.log(appointment)
+            const result = await appointmentsCollection.insertOne(appointment)
+            res.json(result)
+        })
+
 
     } finally {
         // await client.close();
